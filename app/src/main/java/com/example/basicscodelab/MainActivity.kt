6bbad4d95551f97221980e790cd3d194d5a1f840
OYJ <oyj7677@gmail.com>
2024-02-28 00:39:54 +0900

    Design: Apply custom BasicsCodelabTheme
    1. Color.kt에 색상 리소스 추가.
    2. Theme.kt의 DarkColorScheme, LightColorScheme 값 변경
    3. 프리뷰의 BasicsCodelabTheme에 (dynamicColor = false) 추가.

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -18,7 +18,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,14 +26,15 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
-import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
+import androidx.compose.ui.text.font.FontWeight
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BasicsCodelabTheme {
+            BasicsCodelabTheme(dynamicColor = false) {
                 MyApp(modifier = Modifier.fillMaxSize())
             }
         }
@@ -76,7 +76,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello")
-                Text(text = name)
+                Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold
+                ))
             }
             ElevatedButton(
                 onClick = { expanded = !expanded }
@@ -135,18 +137,23 @@ fun OnboardingPreview() {
     }
 }
 
-@Preview(showBackground = true, widthDp = 320)
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "GreetingPreviewDatk"
+)
 @Composable
 fun GreetingPreview() {
-    BasicsCodelabTheme {
-        MyApp()
+    BasicsCodelabTheme(dynamicColor = false) {
+        Greetings()
     }
 }
 
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingsPreview() {
-    BasicsCodelabTheme {
+    BasicsCodelabTheme(dynamicColor = false) {
         Greetings()
     }
 }
